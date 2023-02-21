5cfa870ba5abf002d15e679c72d5c4265e43e0c9
DaurenPP2 <d_kozhakhmetov@kbtu.kz>
2023-02-21 21:45:40 +0600

    Lab4

---
+++ Lab4/angular-naxzd3/src/app/products.ts
@@ -0,0 +1,91 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  photo:string;
+  site:string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Iphone 14 pro Max',
+    price: 712000,
+    description: 'A large phone with one of the best screens 256gb',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h82/h83/62948396662814/apple-iphone-14-pro-128gb-fioletovyj-106363319-1.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000'
+  },
+  {
+    id: 2,
+    name: 'Iphone 14 pro',
+    price: 655111,
+    description: 'A great phone with one of the best cameras 256gb',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h69/h10/63073435189278/apple-iphone-14-pro-128gb-cernyj-106363265-1.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-chernyi-106363265/?c=750000000'
+
+  },
+  {
+    id: 3,
+    name: 'Iphone 14',
+    price: 427150,
+    description: '128gb Blue',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h93/h56/63073057112094/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000'
+  },
+  {
+    id: 4,
+    name: 'Iphone 13 Pro Max',
+    price: 669355,
+    description: 'A great phone with one of the best cameras 128gb green',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/he6/h64/49319418495006/apple-iphone-13-pro-max-256gb-zelenyj-104079153-1.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-13-pro-max-128gb-zelenyi-104079084/?c=750000000'
+  },
+  {
+    id: 5,
+    name: 'Iphone 13 Pro',
+    price: 693995,
+    description: '256gb Blue',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h49/hf4/46393529761822/apple-iphone-13-pro-256gb-goluboj-102298652-1-Container.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-13-pro-256gb-goluboi-102298652/?c=750000000'
+  },
+  {
+    id: 6,
+    name: 'Iphone 13',
+    price: 380700,
+    description: '128gb Midnight',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hc2/h05/46392662458398/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000'
+  },
+  {
+    id: 7,
+    name: 'Iphone 14+',
+    price: 502689,
+    description: '128gb Blue',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h93/h56/63073057112094/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-14-plus-128gb-goluboi-106363220/?c=750000000'
+  },
+  {
+    id: 8,
+    name: 'Iphone 13 mini',
+    price: 446000,
+    description: '256gb Midnight; very small',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/hc2/h05/46392662458398/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-13-mini-128gb-chernyi-102298500/?c=750000000'
+  },
+  {
+    id: 9,
+    name: 'Iphone 12 Pro max',
+    price: 615214,
+    description: '128gb Gold',
+    photo:'https://resources.cdn-kaspi.kz/medias/sys_master/images/images/h08/h34/32806615121950/apple-iphone-12-pro-max-128gb-zolotistyj-100658110-1-Container.jpg',
+    site:'https://kaspi.kz/shop/p/apple-iphone-12-pro-max-128gb-zolotistyi-100658110/?c=750000000'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
